5053dcca9613af43d03fef318bad457be338bd7b
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 18:48:37 -0300

    Correcoes na saida das strings.

--- FIAP /Cap_01_Variaveis/Variaveis_01.py
+++ FIAP /Cap_01_Variaveis/Variaveis_01.py
@@ -8,14 +8,14 @@ qtde_funcionarios = int(input('Quantidade de funcionarios: '))
 media_Mensalidade = float(input('Media da mensalidade: '))
 
 print(nome + ' trabalha na empresa ' + empresa + '.')
-print('Possui: ' + qtde_funcionarios + ' funcionarios.')
+print('Possui: ' + str(qtde_funcionarios) + ' funcionarios.')
 print('Valor medio da mensalidade: R$' + str(media_Mensalidade) + '.')
 print('== Verificando o tipo de dado de cada variavel: ==')
 
-print('Variavel nome: ' + type(nome))
-print('Variavel empresa: ' + type(empresa))
-print('Variavel qtde_funcionarios: ' + type(qtde_funcionarios))
-print('Variavel media_Mensalidade: ' + type(media_Mensalidade))
+print('Variavel nome: ' , type(nome))
+print('Variavel empresa: ',  type(empresa))
+print('Variavel qtde_funcionarios: ', type(qtde_funcionarios))
+print('Variavel media_Mensalidade: ', type(media_Mensalidade))
 
 
 
