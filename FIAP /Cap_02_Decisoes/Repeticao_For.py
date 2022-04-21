2bac021bcd25bafbd145e2586f8a4b762cbbec68
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 19:52:04 -0300

    Anotacoes sobre a estrutura FOR.

--- FIAP /Cap_02_Decisoes/Repeticao_For.py
+++ FIAP /Cap_02_Decisoes/Repeticao_For.py
@@ -4,7 +4,12 @@
 
 # FOR - diferentemente do While, é possivel determinar o inicio, o final da repeticao e seu incremento.
 
+# Variaveis
 tabuada = int(input('Informe o valor da tabuada a ser exibida: '))
 limite = int(input('Ate onde deve ser calculada a tabuada? '))
+
+# Estrutura FOR com a variavel valor
+# metodo range usado juntamente com a estrutura FOR.
+# range(inicio, fim, incremento)
 for valor in range(1, limite + 1, 1):
     print(tabuada, ' X ', valor, ' = ', tabuada * valor)
