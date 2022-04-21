510601df1fe1a0a8815b5bfeb1186cea6fc75b8f
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 19:10:02 -0300

    Anotacoes...

--- FIAP /Cap_02_Decisoes/Decisao_Simples.py
+++ FIAP /Cap_02_Decisoes/Decisao_Simples.py
@@ -4,9 +4,10 @@
 
 # Decisao simples - se, satisfeita a condicao, o fluxo do programa vai entrar no bloco e ira executar o que estiver dentro.
 
-
+# Variaveis
 nome = input('Informe o nome: ')
 idade = int(input('Informe a idade: '))
 
+# Estrutura de decisao simples
 if idade >= 65:
     print('O paciente ' + nome + ' possui atendimento prioritario.')
