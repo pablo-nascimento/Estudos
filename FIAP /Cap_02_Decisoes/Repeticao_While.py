9761f2b11ffd81051137cbe2648c8b40fa2e3bef
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 19:35:20 -0300

    Estruturas de Repeticao. Repeticao usando o While.

---
+++ FIAP /Cap_02_Decisoes/Repeticao_While.py
@@ -0,0 +1,10 @@
+# Curso Python - FIAP
+# Estruturas de Repeticao - Repeticao While
+# 21.04.2022 - 19h31
+
+num = int(input('Informe um numero: '))
+
+while num < 100:
+    print('\t', num)
+    num = num + 1
+print('Repeticao finalizada.')
