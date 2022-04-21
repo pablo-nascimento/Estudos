a7deb92d09df3d503a22f5c02ad7819c41ffcd09
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 20:05:39 -0300

    Anotacoes sobre a lista.

--- FIAP /Cap_03_Listas/Listas_01.py
+++ FIAP /Cap_03_Listas/Listas_01.py
@@ -2,11 +2,18 @@
 # Variaveis e Listas - Introducao
 # 21.04.2022 - 19h57
 
+# Declarando a lista
 inventario = []
+
+# Variavel do tipo string usada para controlar o laco.
 resposta = 'S'
 while resposta == 'S':
+
+    # Metodo append usado para alimentar a lista.
     inventario.append(input('Equipamento: '))
     inventario.append(float(input('Valor: ')))
     inventario.append(int(input('Numero Serial: ')))
     inventario.append(input('Departamento: '))
-    resposta = input('Digite \"S\" para continuar:  ')
+
+    # Variavel de controle do laco
+    resposta = input('Digite \"S\" para continuar:  ').upper()
