6b305293bb947747505fedd007eaab84eda01eb7
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 19:37:20 -0300

    Adicionando anotacoes sobre a repeticao com While.

--- FIAP /Cap_02_Decisoes/Repeticao_While.py
+++ FIAP /Cap_02_Decisoes/Repeticao_While.py
@@ -2,9 +2,11 @@
 # Estruturas de Repeticao - Repeticao While
 # 21.04.2022 - 19h31
 
+# Variavel usada para controlar o laco
 num = int(input('Informe um numero: '))
 
+# Condicao para que o laco seja executado, ate um certo ponto.
 while num < 100:
     print('\t', num)
-    num = num + 1
+    num = num + 1 # Incremento, usado para que a repeticao ocorra, ate que a condicao acima, seja satisfeita.
 print('Repeticao finalizada.')
