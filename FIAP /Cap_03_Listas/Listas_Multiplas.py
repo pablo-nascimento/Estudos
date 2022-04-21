b565533f7cf37db1525a962a95bc021eda0b6708
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-21 20:31:26 -0300

    Mais anotacoes, agora, sobre as listas multiplas.

--- FIAP /Cap_03_Listas/Listas_Multiplas.py
+++ FIAP /Cap_03_Listas/Listas_Multiplas.py
@@ -2,12 +2,14 @@
 # Listas Multiplas
 # 21.04.2022 - 20h14
 
+# Declarando as listas
 equipamento = []
 valor = []
 serial = []
 departamento = []
 resposta = 'S'
 
+# Estrutura usada para alimentar as listas
 while resposta == 'S':
     equipamento.append(input('Informe o nome: '))
     valor.append(float(input('Informe o valor: ')))
@@ -15,6 +17,7 @@ while resposta == 'S':
     departamento.append(input('Informe o departamento: '))
     resposta = input('Digite \"S"\ para continuar: ').upper()
 
+# Utilizando o metodo FOR para percorrer e dar a saida do conteudo da lista.
 for i in range(0, len(equipamento)):
     print('\nEquipamento: ', (i + 1))
     print('Nome: ', equipamento[i])
