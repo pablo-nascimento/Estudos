6d56cf07640a87a366f6f6f906f9051d2eb0a795
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 14:35:08 -0300

    Declarando um dicionario.

--- FIAP /Cap_04_Dicionarios/Dicionarios_01.py
+++ FIAP /Cap_04_Dicionarios/Dicionarios_01.py
@@ -2,4 +2,13 @@
 # Criação de Dicionários
 # 23.04.2022 - 14h27
 
+# Declarando um dicionario
+# Estrutura de dados que funciona com o conjunto chave e valor
+# dicionario = {chave : valor - que pode ser uma lista, ou um outro valor}
+
+usuarios = {
+        'user_01' : ['usuario_01', '23.12.2021', 'TI_01'],
+        'user_02' : ['usuario_02', '27.12.2021', 'ADM_02']
+        }
+
 
