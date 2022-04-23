b09b9e23eae1775acb0f2031725bac1f52947aa2
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 14:56:33 -0300

    Adicionando a parte de inserir novos dados.

--- FIAP /Cap_04_Dicionarios/Dicionarios_02.py
+++ FIAP /Cap_04_Dicionarios/Dicionarios_02.py
@@ -2,3 +2,31 @@
 # Manipulando informações dentro de um dicionário
 # 23.04.2022 - 14h48
 
+users = {}
+
+escolha = (
+    input('Escolha uma das opções abaixo: ' +
+          'I - Inserir um novo usuário: ' +
+          'P - Pesquisar um usuário: ' +
+          'E - Excluir um usuário: ' +
+          'L - Listar um usuário: '
+          ).upper()
+)
+
+
+while escolha == 'I' or escolha == 'P' or escolha == 'E' or escolha == 'L':
+    if escolha == 'I':
+        chave = input('Login: ')
+        usuario = input('Nome do usuário: ')
+        data_acesso = input('Último acesso (xx.xx.xxxx): ')
+        estacao = input('Estação de trabalho: ')
+        users[chave] = [usuario, data_acesso, estacao]
+    escolha = (
+        input('Escolha uma das opções abaixo: ' +
+              'I - Inserir um novo usuário: ' +
+              'P - Pesquisar um usuário: ' +
+              'E - Excluir um usuário: ' +
+              'L - Listar um usuário: '
+              ).upper()
+    )
+
