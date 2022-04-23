83f8bb281e8eb524be66cb13e75c1f1c3801b3ab
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 14:38:49 -0300

    Adicionando dados ao dicionario depois de sua criação.

--- FIAP /Cap_04_Dicionarios/Dicionarios_01.py
+++ FIAP /Cap_04_Dicionarios/Dicionarios_01.py
@@ -6,9 +6,14 @@
 # Estrutura de dados que funciona com o conjunto chave e valor
 # dicionario = {chave : valor - que pode ser uma lista, ou um outro valor}
 
-usuarios = {
+usuarios = \
+    {
         'user_01' : ['usuario_01', '23.12.2021', 'TI_01'],
         'user_02' : ['usuario_02', '27.12.2021', 'ADM_02']
-        }
+    }
+
+# Adicionando novos registros ao dicionario acima criado
+# nome_dicionario[chave] = valor
+usuarios['user_03'] = ['usuario_03', '01.01.2022', 'FIN_05']
 
 
