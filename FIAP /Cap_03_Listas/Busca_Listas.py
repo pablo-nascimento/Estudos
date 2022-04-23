8314c3f5e7a5199856c92de6373e562357172068
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 10:14:35 -0300

    Alimentando as listas.

--- FIAP /Cap_03_Listas/Busca_Listas.py
+++ FIAP /Cap_03_Listas/Busca_Listas.py
@@ -2,3 +2,18 @@
 # Listas Multiplas - Buscando informacoes em listas
 # 23.04.2022 - 10h05
 
+# Definindo as listas
+equipamento = []
+valor = []
+serial = []
+departamento = []
+resposta = 'S'
+
+# Alimentando as listas acima
+while resposta == 'S':
+    equipamento.append(input('Informe o equipamento: '))
+    valor.append(input('Informe o valor: '))
+    serial.append(input('Informe o serial: '))
+    departamento.append(input('Informe o departamento: '))
+    resposta = input('Deseja inserir mais algum equipamento? S/N')
+
