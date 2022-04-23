84411c369a3e9aa4b6e76d009e1414149ef7ac02
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 16:18:01 -0300

    Criado este arquivo .py para focar no estudo de como manipular de diversas formas, um dicionario de dados em Python.

---
+++ FIAP /Cap_04_Dicionarios/Dicionario_Teste.py
@@ -0,0 +1,13 @@
+# Curso Python - FIAP
+# Arquivo de teste e treino para aperfeiçoar o uso com dicionários
+# 23.04.2022 - 16h02
+
+# Declarando um dicionario.
+estado = {'SP': ['Sao Paulo', 'Guarulhos, Campinas, Santos, Araçatuba, Sao José do Rio Preto'],
+          'RJ': ['Rio de Janeiro', 'Nova Iguaçu, Petrópolis, Duque de Caxias, Angra dos Reis, Paraty'],
+          'BA': ['Salvador', 'Feira de Santana, Camaçari, Caculé, Igaci, Teolandia'],
+          'ES': ['Vitoria', 'Marataizes, Vargem Alta, Vila Velha, Serra, Linhares']
+        }
+
+estado['PR'] = ['Curitiba', 'Almirante Tamandare, Maringa, Londrina, Ponta Grossa, Paranagua']
+
