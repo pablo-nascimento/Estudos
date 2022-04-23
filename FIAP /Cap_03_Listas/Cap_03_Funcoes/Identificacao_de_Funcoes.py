8a053a54a4d6665309937f76715af82fe4c22480
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-23 12:48:57 -0300

    Adicionando as funções.

--- FIAP /Cap_03_Listas/Cap_03_Funcoes/Identificacao_de_Funcoes.py
+++ FIAP /Cap_03_Listas/Cap_03_Funcoes/Identificacao_de_Funcoes.py
@@ -7,7 +7,8 @@
 #       bloco de código a ser executado.
 #       palavra reservada return (caso seja necessário retornar algum valor).
 
-def preenchendoInventario(lista):
+# Alimentando a lista.
+def preencherInventario(lista):
     while resposta == 'S':
         equipamento = [
             input('Equipamento: '),
@@ -18,4 +19,50 @@ def preenchendoInventario(lista):
         lista.append(equipamento)
         resposta = input('Deseja adicionar mais algum equipamento? ').upper()
         print('\t')
+
+# Mostrando o conteudo do que já está salvo nas listas.
+def exibirInventario(lista):
+    for elemento in lista:
+        for indice in elemento:
+            print(indice)
+        print('\t')
+
+# Fazendo a busca na lista.
+def localizarLista(lista):
+    pesquisa = input("Informe o equipamento a ser localizado: ")
+    for elemento in lista:
+        for indice in elemento:
+            if pesquisa == elemento[0]:
+                print(indice)
+
+# Calculando a depreciaçao, onde, será alterado o valor da lista que já foi pre definido.
+def calcularDepreciacao(lista, taxa):
+    depreciacao = input("Informe o nome do equipamento a ser depreciado: ")
+    for elemento in lista:
+        if depreciacao == elemento[0]:
+            print('Equipamento: ', elemento[0])
+            print("Valor atual: ", elemento[1])
+            elemento[1] = elemento[1] * (1-(taxa / 100))
+
+# Exclusão de itens.
+def excluirInventario(lista):
+    serial = int(input('Informe o serial do equipamento a ser excluído: '))
+    for elemento in lista:
+        if serial == elemento[2]:
+            lista.remove(elemento)
+    return 'Itens Excluídos.'
+
+# Aplicando as funcoes matematicas aprendidas anteriormente.
+def mostrarResumoValores(lista):
+    valores = []
+    for elemento in lista:
+        valores.append(elemento[1])
+    print('Valor mais caro: ', max(valores))
+    print('Valor mais barato: ', min(valores))
+    print('Soma dos valores: ', sum(valores))
+
+
+
+
+
 
