a2974ad17bd9016a95612a990e43eb70a0bbe6b1
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-04 20:26:07 -0300

    Iniciando o estudo com tuplas.

---
+++ FIAP /Cap_04_Dicionarios/Tuplas.py
@@ -0,0 +1,8 @@
+# Curso Python - FIAP
+# Trabalhando com Tuplas.
+# 04.05.2022 - 20h23
+
+
+usuarios = {} # Dicionario de usuários
+email = ['email@123email.com', 'email@321email.com'] # Lista de e-mails.
+
