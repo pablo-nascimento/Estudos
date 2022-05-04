4a5a5f560d94b59e5e006f2735b6683e00f62db5
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-04 20:31:02 -0300

    Funcao enumerate.

--- FIAP /Cap_04_Dicionarios/Tuplas.py
+++ FIAP /Cap_04_Dicionarios/Tuplas.py
@@ -6,3 +6,10 @@
 usuarios = {} # Dicionario de usuários
 email = ['email@123email.com', 'email@321email.com'] # Lista de e-mails.
 
+# Funcao enumerate - numerar cada item da lista email e gera uma tupla, para cada operacao.
+# Funcao list - transformar cada tupla retornada pela funcao enumerate como, um item de uma lista.
+# Código abaixo, gera uma lista de tuplas. 
+t = list(enumerate(email))
+
+
+print(t)
