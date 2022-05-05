da438fdcbdb74670250653fee3580a584df77469
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-04 21:29:50 -0300

    Usando a repeticao em conjunto com a funcao readlines para ler o arquivo, linha a linha.

--- FIAP /Cap_05_Arquivos/Arquivos_02.py
+++ FIAP /Cap_05_Arquivos/Arquivos_02.py
@@ -7,6 +7,12 @@ with open("arquivo_estudo.txt", 'r') as arquivo:
     abrir = arquivo.read() # Atribuindo o conteudo do arquivo a variavel abrir
     print(abrir)
 
+# Lendo somente a primeira linha - feito somente para teste.
 with open("arquivo_estudo.txt", "r") as a:
     abrir = a.readline() # funcao readline, so acessa a primeira linha do arquivo.
     print(abrir)
+
+# Lendo o arquivo linha a linha.
+with open("arquivo_estudo.txt", "r") as arquivo:
+    for linha in arquivo.readlines():
+        print(linha)
