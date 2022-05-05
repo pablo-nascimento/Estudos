4b1f1acfe46a7aacd54f996f2f9b6352da0e851a
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-04 21:04:45 -0300

    Criando arquivos, lendo, e gravando dados.

--- FIAP /Cap_05_Arquivos/Arquivos_01.py
+++ FIAP /Cap_05_Arquivos/Arquivos_01.py
@@ -2,3 +2,11 @@
 # Criação e Manipulação de Arquivos.
 # 04.05.2022 - 20h58
 
+# Criação de arquivo - variavel = funcao open(caminho do arquivo, modo de abertura(read-leitura, write-escrita,
+# append(r + w))
+# x-exclusivo(somente quem abriu o arquivo, que pode ve-lo.
+arquivo = open('arquivo_estudo.txt', 'w')
+
+# Gravando dados no arquivo com a funcao write
+arquivo.write("Gravando informacões no arquivo de estudo.")
+arquivo.close() # Fechando o acesso ao arquivo.
