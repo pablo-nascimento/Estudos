52f536dde4ec6c5ba9e390aa710aabc14b44ca1e
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-09 21:24:25 -0300

    Manipulando strings.

---
+++ FIAP /Cap_05_Arquivos/strings.py
@@ -0,0 +1,22 @@
+# Curso Python - FIAP
+# Manipulacao de Strings
+# 09.05.2022 - 21h20
+
+# Declarando uma string
+txt = "Isto e uma string. Isto tambem."
+
+# Acessando um range de posicoes dentro da lista de strings e dando a sua saída.
+# Pegando do começo até o ponto.
+print(txt[0:18])
+
+# Indo da posicao 19 até o final.
+print(txt[19:])
+
+# Acessando com indices negativos. A primeira posicao, é a ultima letra, no caso.
+print(txt[-12:])
+
+# indo da posicao 0 até a 18 dando a saida de 3 em 3 posicoes.
+print(txt[0:18:3])
+#posicao_inicial da string : quantidade de informacao que se vai percorrer : passo.
+
+
