4452c71f79db5bd69b9fb0f4ff054b9b6da6a49a
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-16 22:14:26 -0300

    Anotacoes.

--- FIAP /Cap_05_Arquivos/Arquivos_03.py
+++ FIAP /Cap_05_Arquivos/Arquivos_03.py
@@ -2,10 +2,12 @@
 # Leitura de Arquivos e transformação de dados em lista
 # 16.05.2022 - 22h10
 
-base_dados = []
+base_dados = [] # Lista Vazia
 
-with open("iris.data", "r") as arquivo:
-    for dado in arquivo.readlines():
-        base_dados.append(dado.split(" "))
+with open("iris.data", "r") as arquivo: # Abrindo o arquivo no modo leitura.
+    for dado in arquivo.readlines(): # Percorrendo linha a linha dentro do arquivo.
+        base_dados.append(dado.split(" ")) # Adicionando cada linha do arquivo a lista base_dados
+        # Está sendo usado como separador, o espaço contido no final de cada linha.
+        
 
 print(base_dados)
