795ee5d26846515fef98ad9b62c67664e421bcf0
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-16 22:20:49 -0300

    Mais anotacoes sobre o separador, e o acesso manual a uma determinada posicao da lista criada.

--- FIAP /Cap_05_Arquivos/Arquivos_03.py
+++ FIAP /Cap_05_Arquivos/Arquivos_03.py
@@ -6,8 +6,9 @@ base_dados = [] # Lista Vazia
 
 with open("iris.data", "r") as arquivo: # Abrindo o arquivo no modo leitura.
     for dado in arquivo.readlines(): # Percorrendo linha a linha dentro do arquivo.
-        base_dados.append(dado.split(" ")) # Adicionando cada linha do arquivo a lista base_dados
-        # Está sendo usado como separador, o espaço contido no final de cada linha.
-        
+        base_dados.append(dado.split(",")) # Adicionando cada linha do arquivo a lista base_dados
+        # Está sendo usado como separador, a virgula contida no final de cada linha.
+        # Entao, cada numero, separado por virgula, vira uma posicao dentro da lista base_dados.
 
 print(base_dados)
+print(base_dados[0][2]) # Acessando a terceira posicao da lista interna. Primeira posicao da lista externa.
