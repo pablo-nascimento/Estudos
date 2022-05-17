ea2d198c65c4abd8d9a0fb3b96e2a0cf3fe503cf
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-16 21:57:11 -0300

    Estudando mais sobre strings.

---
+++ FIAP /Cap_05_Arquivos/strings_02.py
@@ -0,0 +1,7 @@
+# Curso Python - FIAP
+# Manipulacao de Strings - 02
+# 16.05.2022 - 21h53
+
+texto = 'Python é legal.'
+
+print(texto.find('a')) # Retorna a primeira ocorrencia do que foi passado por parametro
