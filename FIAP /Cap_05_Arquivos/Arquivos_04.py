8b6ab2c012226a7ea51be5c0129c9151dcd75e49
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-05-25 21:01:19 -0300

    Anotacoes sobre manipular JSON em Python.

--- FIAP /Cap_05_Arquivos/Arquivos_04.py
+++ FIAP /Cap_05_Arquivos/Arquivos_04.py
@@ -2,9 +2,9 @@
 # Leitura de Arquivos JSON
 # 25.05.2022 - 20h38
 
-import json
+import json # Importando módulo JSON
 
-with open("db_users.json", "r") as arquivo_json:
-    arquivo = json.load(arquivo_json)
-    for chave, valor in arquivo.items():
-        print(chave + " - " + str(valor))
+with open("db_users.json", "r") as arquivo_json: # Abrindo o arquivo JSON
+    arquivo = json.load(arquivo_json) # Carregando o que está no arquivo JSON, para a variavel arquivo.
+    for chave, valor in arquivo.items(): # Percorrendo o dicionario arquivo para ter o retorno de chave e valor.
+        print(chave + " - " + str(valor)) # Saída das chaves e de seus valores.
