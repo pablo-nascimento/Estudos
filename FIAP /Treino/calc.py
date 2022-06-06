49227f600460f10041fdfc438c14d4a6c6d8758e
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-06-06 20:39:55 -0300

    Calculadora basica com as 4 operacoes usando dois numeros.

--- FIAP /Treino/calc.py
+++ FIAP /Treino/calc.py
@@ -2,4 +2,35 @@
 # Treino - Calculadora
 # 06.06.2022 - 20h12
 
+n1 = int(input("Entre com o primeiro número: "))
+n2 = int(input("Entre com o segundo número: "))
+operacao = int(input("1 - Soma: \n" +
+                     "2 - Subtração: \n" +
+                     "3 - Multiplicaçao: \n" +
+                     "4 - Divisão: "))
+
+if operacao == 1:
+    print("Soma: ", n1, " + ", n2, " = " ,(n1 + n2))
+elif operacao == 2:
+    if n1 > n2:
+        print("Subtração: ", n1, " - ", n2, " = ", (n1 - n2))
+    elif n2 > n1:
+        print("Subtração: ", n2, " - ", n1, " = ", (n2 - n1))
+elif operacao == 3:
+    print("Multiplicação: ", n1, " + ", n2, " = ", (n1 * n2))
+elif operacao == 4:
+    if n1 > n2:
+        print("Divisão: ", n1, " + ", n2, " = ", (n1 / n2))
+    elif n2 > n1:
+        print("Divisão: ", n1, " + ", n2, " = ", (n2 / n1))
+    elif n1 == n2:
+        print("Os dois números informados são iguais.")
+print("Calculadora finalizada...")
+
+
+
+
+
+
+
 
