1a53a75c56729f9575e327a90e22a9c44ac7b32e
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-06-06 20:14:22 -0300

    Relembrando o conteudo ja estudado. Declarando variaveis.

---
+++ FIAP /Treino/variavel.py
@@ -0,0 +1,9 @@
+# Curso Python - FIAP
+# Treino - Varíaveis
+# 06.06.2022 - 20h05
+
+# Declaração - Variáveis
+a = 0 # inteiro
+b = 1
+texto = "Salvando varíavel do tipo string em Python.." # string
+a2 = 2.55 # float
