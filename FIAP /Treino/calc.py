fa57f48eb4dc68df51626cf08d0b012e57a11e6a
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-06-06 21:05:05 -0300

    Estrutura de repeticao adicionada para executar conforme solicitacao do usuario.

--- FIAP /Treino/calc.py
+++ FIAP /Treino/calc.py
@@ -2,30 +2,32 @@
 # Treino - Calculadora
 # 06.06.2022 - 20h12
 
-n1 = int(input("Entre com o primeiro número: "))
-n2 = int(input("Entre com o segundo número: "))
-operacao = int(input("1 - Soma: \n" +
-                     "2 - Subtração: \n" +
-                     "3 - Multiplicaçao: \n" +
-                     "4 - Divisão: "))
-
-if operacao == 1:
-    print("\nSoma: ", n1, " + ", n2, " = " ,(n1 + n2))
-elif operacao == 2:
-    if n1 > n2:
-        print("\nSubtração: ", n1, " - ", n2, " = ", (n1 - n2))
-    elif n2 > n1:
-        print("\nSubtração: ", n2, " - ", n1, " = ", (n2 - n1))
-elif operacao == 3:
-    print("\nMultiplicação: ", n1, " * ", n2, " = ", (n1 * n2))
-elif operacao == 4:
-    if n1 > n2:
-        print("\nDivisão: ", n1, " / ", n2, " = ", (n1 / n2))
-    elif n2 > n1:
-        print("\nDivisão: ", n1, " / ", n2, " = ", (n2 / n1))
-    elif n1 == n2:
-        print("\nOs dois números informados são iguais.")
-print("\nCalculadora finalizada...")
+resp = input("Para realizar a operacao, tecle S: ") # Variavel responsavel por controlar o laco
+while resp == "S" or resp == "s":
+    n1 = int(input("Entre com o primeiro número: "))
+    n2 = int(input("Entre com o segundo número: "))
+    operacao = int(input("1 - Soma: \n" +
+                         "2 - Subtração: \n" +
+                         "3 - Multiplicaçao: \n" +
+                         "4 - Divisão: "))
+    if operacao == 1:
+        print("\nSoma: ", n1, " + ", n2, " = " ,(n1 + n2))
+    elif operacao == 2:
+        if n1 > n2:
+            print("\nSubtração: ", n1, " - ", n2, " = ", (n1 - n2))
+        elif n2 > n1:
+            print("\nSubtração: ", n2, " - ", n1, " = ", (n2 - n1))
+    elif operacao == 3:
+        print("\nMultiplicação: ", n1, " * ", n2, " = ", (n1 * n2))
+    elif operacao == 4:
+        if n1 > n2:
+            print("\nDivisão: ", n1, " / ", n2, " = ", (n1 / n2))
+        elif n2 > n1:
+            print("\nDivisão: ", n1, " / ", n2, " = ", (n2 / n1))
+        elif n1 == n2:
+            print("\nOs dois números informados são iguais.")
+    print("\nCalculadora finalizada...")
+    resp = input("Deseja realizar mais alguma operacao: ").upper()
 
 
 
