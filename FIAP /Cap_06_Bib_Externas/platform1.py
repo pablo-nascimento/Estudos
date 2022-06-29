dc188d9aeafefa112de221fa81ab10b684219885
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-06-28 21:32:24 -0300

    Uso da biblioteca Datetime.

--- FIAP /Cap_06_Bib_Externas/platform1.py
+++ FIAP /Cap_06_Bib_Externas/platform1.py
@@ -4,10 +4,19 @@
 
 import platform
 # Biblioteca Python - usando para acesso a informacoes do sistema.
+
+from datetime import datetime
+
 print("Nome da Máquina ............. ", platform.node())
 print("Arquitetura ............. ", platform.architecture())
 print("Nome do SO ............. ", platform.system())
 print("Versao do SO ............. ", platform.release())
 print("Processador ............. ", platform.processor())
 print("Python .......... ", platform.python_version())
+print("\n")
 
+# uso da biblioteca datetime
+print(datetime.now())  # Data atual
+print(datetime.now().year) # Somente o ano
+print(datetime.now().month) # Mes
+print(datetime.now().day) # Dia
