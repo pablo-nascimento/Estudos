68ea17bc554059be9949872a6d783aef6418576b
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-06-28 21:26:39 -0300

    Uso de diversos comandos usando a biblioteca Platform.

--- FIAP /Cap_06_Bib_Externas/platform1.py
+++ FIAP /Cap_06_Bib_Externas/platform1.py
@@ -3,4 +3,11 @@
 # 28.06.2022 - 21h11
 
 import platform
+# Biblioteca Python - usando para acesso a informacoes do sistema.
 print("Nome da Máquina ............. ", platform.node())
+print("Arquitetura ............. ", platform.architecture())
+print("Nome do SO ............. ", platform.system())
+print("Versao do SO ............. ", platform.release())
+print("Processador ............. ", platform.processor())
+print("Python .......... ", platform.python_version())
+
